b048854591bc6a976f4449ff4e74a2eb0306980d
Jose Alejandro Montana <jamontanac@unal.edu.co>
2022-11-06 20:08:08 -0500

    Adding the project working with the dockerfile ready to deploy

---
+++ Mid Project/Readme.md
@@ -0,0 +1,12 @@
+# Mid project
+Here i present my mid project in which i have taken the data of Santander bikes in UK from 1th jannuary to 28 of february, This project is simple and what it tried to do was to predict the demand of bikes at certain hour based in only weather conditions and the fact that the day was a workday or not. The data of Weather conditions was extracted for a free API.
+
+To check out the process in which i start buyilding the data as well as exploring the data and then making a model can be found in this repo
+
+* [Building the Data and EDA](https://github.com/jamontanac/MLZoomCamp/blob/main/Mid%20Project/Data%20exploring.ipynb)
+
+* [Building the models and Exporting to Bento](https://github.com/jamontanac/MLZoomCamp/blob/main/Mid%20Project/ModelingData.ipynb)
+
+* [Serving the model](https://github.com/jamontanac/MLZoomCamp/blob/main/Mid%20Project/service.py)
+
+* [containerize the model]()
--- Mid Project/service.py
+++ Mid Project/service.py
@@ -1,9 +1,9 @@
 import bentoml
 from bentoml.io import JSON, NumpyNdarray, PandasDataFrame
-from pydantic import BaseModel
-from typing import List, Union
 import pandas as pd
-import numpy as np
+
+
+# Here we define the variables to use in the prediction
 numerical=["hour","temp","feelslike","dew","humidity","cloudcover","visibility"]
 categorical=["WorkDay","Holiday","conditions"]
 col_names=numerical+categorical
@@ -15,43 +15,18 @@ model=bentoml.sklearn.get("demand_prediction_service:tvopn7c3t6pohqr2")
 model_runner = model.to_runner()
 svc = bentoml.Service("demand_prediction_model",runners=[model_runner],)
 
-
-
-
-
-# class predictionRecord(BaseModel):    
-#     hour: int = 0
-#     temp: float = 7.3
-#     feelslike: float = 7.3
-#     dew: float = 5.1
-#     humidity: float = 85.95
-#     cloudcover: float = 62.6
-#     visibility: float = 12.2
-#     WorkDay: bool = False
-#     Holiday: bool = False
-#     conditions: str = "Partially cloudy"
-
-# class DemandApp(BaseModel):
-#     records: List[predictionRecord]
-
- 
-
-# @svc.api(input=JSON(pydantic_model=DemandApp), output=JSON())
-# @svc.api(input=JSON(), output=JSON())
-#@svc.api(input=NumpyNdarray(shape=(-1,10),enforce_shape=True), output=JSON())
-#@svc.api(input=input_spec, output=JSON())
 #Here we need to use pandas dataframe since the predict function takes a 2D array as input
-#
+#so we provide the type data that it accepts
 @svc.api(input=PandasDataFrame(
     columns=col_names,
     orient="records",
     enforce_shape=True,
-    dtype={"hour":int,"temp":float,"feelslike":float,"dew":float,"humidity":float,"cloudcover":float,"visibility":float,"WorkDay":bool,"Holiday":bool,"conditions":str},
+    #dtype={"hour":int,"temp":float,"feelslike":float,"dew":float,"humidity":float,"cloudcover":float,"visibility":float,"WorkDay":bool,"Holiday":bool,"conditions":str},
     enforce_dtype=False,
     shape=(-1,10),
 ), output=JSON())
-def predict(data: pd.DataFrame):
+async def predict(data: pd.DataFrame):
     #print(data,type(data))
-    prediction = model_runner.predict.run(data)
+    prediction = await model_runner.predict.async_run(data)
     return {"prediction":prediction}
 
